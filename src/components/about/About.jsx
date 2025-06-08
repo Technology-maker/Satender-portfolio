a454247d51f04c368b2287360aa5e963d6f74141
Technology-maker <ys5401519@gmail.com>
2025-06-08 16:20:04 +0530

    pic updates

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import TypeEffect from './TypeEffect'
+import mywebimg from '../../assets/my-image-logo/myimg.png'
 
 const About = () => {
   return (
@@ -29,7 +30,7 @@ const About = () => {
       {/* My Image */}
       <div className="md:w-1/2 w-full flex justify-center items-center p-4 min-h-[220px]">
         <img
-          src="../../assets/my-image-logo/mywebimg.jpg"
+          src={mywebimg}
           alt="Satender"
           className="rounded-full w-40 h-40 md:w-72 md:h-72 object-cover border-2 border-solid border-sky-400  transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-105 shadow-2xl shadow-indigo-500/50 "
         />
