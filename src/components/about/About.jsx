065e29a0ae1e86ad02c6d31fe425022c9877bf65
Technology-maker <ys5401519@gmail.com>
2025-07-17 08:27:04 +0530

    Add Some Modification

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,7 +6,7 @@ import { MdFileDownload } from "react-icons/md"
 const About = () => {
   return (
     <section id='about' className="py-0 flex flex-col md:flex-row items-center justify-center mt-1 bg-[#A7A2A9] rounded-md min-h-[450px]">
-      
+
       {/* About Me Text */}
       <div className="md:w-1/2 w-full p-4 flex items-center justify-center min-h-[220px]">
         <div className="w-full max-w-xl">
@@ -24,12 +24,7 @@ const About = () => {
             Currently pursuing a BCA, I have a strong foundation in full-stack development with hands-on experience in the MERN stack, React.js, and JavaScript. I am also proficient in C, C++, and Data Structures & Algorithms, having solved 50+ problems on LeetCode and HackerRank.
           </p>
           {/* Download Resume Section */}
-          <a
-            href="/public/Satender-CV.pdf"
-            download
-            className="inline-flex items-center gap-2 bg-[#8245ec] text-white px-5 py-2 rounded-md font-semibold shadow hover:bg-[#6d36c9] transition-colors duration-200 border-slate-700 border-2"
-          >
-            <MdFileDownload className="text-xl" />
+          <a href="/Satender-CV.pdf" download className="inline-flex items-center gap-2 bg-[#8245ec] text-white px-5 py-2 rounded-md font-semibold shadow hover:bg-[#6d36c9] transition-colors duration-200 border-slate-700 border-2"> <MdFileDownload className="text-xl" />
             Download Resume
           </a>
         </div>
